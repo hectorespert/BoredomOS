d95609161eb4d8297a5c3d06992dc17ee53d6e7f
Hector Espert <hectorespertpardo@gmail.com>
2020-03-29 20:12:21 +0200

    Initial led task

---
+++ src/led.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+
+void TaskInitLed(void *pvParameters)
+{
+  (void) pvParameters;
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  for (int i = 0; i < 5; i++)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    vTaskDelay( 500 / portTICK_PERIOD_MS );
+    digitalWrite(LED_BUILTIN, LOW);
+    vTaskDelay( 500 / portTICK_PERIOD_MS );
+  }
+
+  vTaskDelete(NULL);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,20 @@
 #define PRIORITY_HIGH 2
 #define PRIORITY_HIGHEST 3
 
-/**
-  Blink task
-*/
-void TaskBlink(void *pvParameters)
-{
-  (void) pvParameters;
 
-  // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  for (;;) // A Task shall never return or exit.
-  {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 500 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 500 / portTICK_PERIOD_MS ); // wait for one second
-  }
-}
+extern void TaskInitLed(void *pvParameters); 
 
 /**
-   Tasks
+   Task handlers
 */
-TaskHandle_t taskBlinkHandler;
+TaskHandle_t taskTaskInitLedHandler;
 
 void setup() {
 
   /**
      Create tasks
   */
-  // Register blink task
-  xTaskCreate(TaskBlink, "Blink", 64, NULL, PRIORITY_HIGHEST,  &taskBlinkHandler);
+  xTaskCreate(TaskInitLed, "InitLed", 64, NULL, PRIORITY_HIGHEST,  &taskTaskInitLedHandler);
 
 }
 
