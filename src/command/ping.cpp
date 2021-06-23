d39a530699736f70e08581cfaeee730cf0267c16
Hector Espert <hectorespertpardo@gmail.com>
2021-06-24 00:18:36 +0200

    Clean code

--- src/command/ping.cpp
+++
@@ -1,7 +0,0 @@
-#include <Arduino.h>
-#include <CmdParser.hpp>
-
-void pingCommand(CmdParser *myParser)
-{
-  Serial.println("OK");
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,5 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
-//#include <semphr.h>
-
-//#include <solarchargershield.h>
-//#include <internaltemperature.h>
-
-//#include <RTClib.h>
 
 #define PRIORITY_LOWEST 0
 #define PRIORITY_LOW 1
@@ -18,85 +12,12 @@
 
 extern void TaskLed(void *pvParameters);
 
-//extern void TaskCommandHandler(void *pvParameters);
-
-//extern void TaskSolarChargerShield(void *pvParameters);
-
-//extern void TaskInternalTemperature(void *pvParameters);
-
-/**
-   Task handlers
-*/
-//TaskHandle_t taskSolarChargerShieldHandler;
-
-//TaskHandle_t taskCommandsHandler;
-
-//TaskHandle_t taskInternalTemperatureHandler;
-
-/**
- * Semaphore
- */
-
-//SemaphoreHandle_t solarChargerShieldMutex;
-
-//SemaphoreHandle_t internalTemperatureMutex;
-
-//SemaphoreHandle_t rtcMutex;
-
-/**
- * Global data
- */
-
-//SolarChargerShield solarChargerShield;
-
-//InternalTemperature internalTemperature;
-
-//RTC_DS3231 rtc;
 
 void setup()
 {
-
-   Serial.begin(9600);
-
-   /**
-    * DS3231 init
-    */
-   //configASSERT(rtc.begin());
-
-   /**
-    * RTC configuration if lost power
-    */
-   //if (rtc.lostPower())
-   //{
-   //   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-   //}
-
-   /**
-    * Create mutex
-    */
-   //solarChargerShieldMutex = xSemaphoreCreateMutex();
-
-   //internalTemperatureMutex = xSemaphoreCreateMutex();
-
-   //rtcMutex = xSemaphoreCreateMutex();
-
-   /**
-    *  Create tasks
-    */
-
-    xTaskCreate(TaskLed, "Led", 128, NULL, PRIORITY_HIGHEST, NULL);
-
-   //xTaskCreate(TaskSolarChargerShield, "SolarCharger", 256, NULL, PRIORITY_HIGH, &taskSolarChargerShieldHandler);
-
-   //xTaskCreate(TaskInternalTemperature, "InternalTemperature", 256, NULL, PRIORITY_HIGH, &taskInternalTemperatureHandler);
-
-   //xTaskCreate(TaskCommandHandler, "Command", 128, NULL, PRIORITY_LOW, &taskCommandsHandler);
+    xTaskCreate(TaskLed, "Led", 128, nullptr, PRIORITY_HIGHEST, nullptr);
 }
 
 void loop()
-{
-}
-
-void vPortSuppressTicksAndSleep(TickType_t xExpectedIdleTime)
 {
 }
