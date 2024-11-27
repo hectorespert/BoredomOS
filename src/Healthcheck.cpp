ab0e1d7f58dcc2a7a588f1dcd97b3a18bee6348f
Hector Espert <hectorespertpardo@gmail.com>
2024-11-27 21:39:30 +0100

    Change health check led

--- src/Healthcheck.cpp
+++ src/Healthcheck.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
 
-#define LED_HIGH_MS 250 / portTICK_PERIOD_MS
-#define LED_LOW_MS (60000 - LED_HIGH_MS) / portTICK_PERIOD_MS
+#define LED_HIGH_MS 250
+#define LED_LOW_MS 59750
 
 [[noreturn]] void TaskHealthCheck(void *pvParameters)
 {
@@ -13,9 +13,9 @@
     for (;;)
     {
         digitalWrite(LED_BUILTIN, HIGH);
-        vTaskDelay(LED_HIGH_MS);
+        vTaskDelay(LED_HIGH_MS / portTICK_PERIOD_MS);
         digitalWrite(LED_BUILTIN, LOW);
-        vTaskDelay(LED_LOW_MS);
+        vTaskDelay(LED_LOW_MS / portTICK_PERIOD_MS);
     }
 
 }
